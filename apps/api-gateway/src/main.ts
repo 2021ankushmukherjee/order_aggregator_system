3094155071023617c489d65c93fe3f6293d16470
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-06-22 22:37:05 +0530

    Enhance API Gateway setup with global prefix, CORS, and Swagger integration

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,42 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
 import { ApiGatewayModule } from './api-gateway.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApiGatewayModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(ApiGatewayModule, {
+    snapshot: true,
+  });
+
+  const globalPreset = process.env.GLOBAL_PRESET || '';
+
+  app.use(express.json({ limit: '50mb' }));
+  app.setGlobalPrefix(globalPreset);
+
+  app.enableCors({
+    origin: '*', // Replace with specific origins in production
+  });
+
+  // Swagger setup (conditionally enabled)
+  if (process.env.SWAGGER_ENABLED?.toLowerCase() === 'true') {
+    const config = new DocumentBuilder()
+      .setTitle('Order Aggregator System')
+      .setDescription('REST APIs for order placement and stock sync')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .addTag('Order')
+      .addTag('Vendor')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(`${globalPreset}/swagger`, app, document);
+  }
+
+  const port = parseInt(process.env.NODE_PORT || '3000', 10);
+  await app.listen(port, () => console.log(`API Gateway is listening on port ${port}`));
 }
+
 bootstrap();
