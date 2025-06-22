93f36c6ee568d6f707fd37dec4d5931fa8367965
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-06-22 22:37:28 +0530

    Add module imports for Order, Vendor, and Database modules in ApiGatewayModule

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
+import { OrderModule } from '@app/order';
+import { VendorModule } from '@app/vendor';
+import { DatabaseModule } from '@app/database';
 
 @Module({
-  imports: [],
+    imports: [OrderModule, VendorModule, DatabaseModule],
   controllers: [ApiGatewayController],
   providers: [ApiGatewayService],
 })
