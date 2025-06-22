7c94795bcffe7910432e834d1e94c73dc8172351
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-06-23 04:52:48 +0530

    Refactor DatabaseModule to remove DatabaseService and configure TypeORM with global configuration

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
-import { DatabaseService } from './database.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConnectionConfig, } from './configuration/orm-config/typeorm.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './configuration';
+
 
 @Module({
-  providers: [DatabaseService],
-  exports: [DatabaseService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        configuration
+      ]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: databaseConnectionConfig,
+    }),
+  ],
+  exports: [TypeOrmModule],
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
