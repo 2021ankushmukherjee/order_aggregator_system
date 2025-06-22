4605e6a9cc7885a2e660db45c39d5b603d908e9e
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-06-23 04:58:28 +0530

    Refactor CommonModule to include InitialService and TypeOrmModule for Product and VendorStock entities

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
+import { InitialService } from './initial.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from '@app/database/entities/product.entity';
+import { VendorStockEntity } from '@app/database/entities/vendor-stock.entity';
 
 @Module({
-  providers: [CommonService],
+  imports: [
+    TypeOrmModule.forFeature([ProductEntity, VendorStockEntity]),
+  ],
+  providers: [CommonService, InitialService],
   exports: [CommonService],
 })
-export class CommonModule {}
+export class CommonModule { }
