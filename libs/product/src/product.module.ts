e2ab45a85b7ac2785944e37249647690d5051c33
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-06-23 05:46:11 +0530

    Add TypeOrmModule and ProductEntity to ProductModule for database integration

--- libs/product/src/product.module.ts
+++ libs/product/src/product.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from '@app/database/entities/product.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ProductEntity]),
+  ],
   providers: [ProductService],
   exports: [ProductService],
 })
