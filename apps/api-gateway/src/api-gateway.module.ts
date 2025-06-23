21aa00b7f7b06ca90411fe2e1fc789b77d95e828
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-06-23 05:56:14 +0530

    Add ProductModule to ApiGatewayModule imports

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -4,9 +4,11 @@ import { VendorModule } from '@app/vendor';
 import { DatabaseModule } from '@app/database';
 import { QueueModule } from '@app/queue';
 import { CommonModule } from '@app/common';
+import { ProductModule } from '@app/product';
 
 @Module({
   imports: [
+    ProductModule,
     OrderModule,
     VendorModule,
     DatabaseModule,
